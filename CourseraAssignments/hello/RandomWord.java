88f0d2fb4708bbc78a504e0069506d23cf5baddd
Jesse Moeller <jessemmoeller@gmail.com>
2021-06-13 16:48:33 -0500

    some reorganization and simplified the solution

--- CourseraAssignments/hello/RandomWord.java
+++ CourseraAssignments/hello/RandomWord.java
@@ -4,8 +4,8 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args) {
-        String champion = StdIn.readString();
-        int i = 2;
+        String champion = "";
+        int i = 1;
         while (!StdIn.isEmpty()) {
             String contender = StdIn.readString();
             if (StdRandom.bernoulli(1.0 / (double) i)) {
