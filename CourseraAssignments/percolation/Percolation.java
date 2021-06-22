1bac8eca556b4a4c5ba6e253cb83594377959b49
Jesse Moeller <jessemmoeller@gmail.com>
2021-06-21 22:00:06 -0500

    perfecting percolation

--- CourseraAssignments/percolation/Percolation.java
+++ CourseraAssignments/percolation/Percolation.java
@@ -2,17 +2,19 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF connections;
     private int size;
-    private boolean[] sites;
+    private boolean[][] siteStatus;
     private int numberOpenSites;
+    private boolean percolates;
 
-    // creates n-by-n grid, with allthis.sites initially blocked
+    // creates n-by-n connections, with all sites initially blocked
     public Percolation(int n) {
         if (n > 0) {
-            this.grid = new WeightedQuickUnionUF(n * n + 2);
+            this.connections = new WeightedQuickUnionUF(n * n);
             this.size = n;
-            this.sites = new boolean[n * n + 2];
+            // (i,j) , (isopen, connectedToTop, connectedTobBttom)
+            this.siteStatus = new boolean[n * n][3];
             this.numberOpenSites = 0;
         }
         else {
@@ -21,11 +23,21 @@ public class Percolation {
     }
 
     private int getIdx(int row, int col) {
-        return (row-1)*this.size+col;
+        return (row - 1) * this.size + (col-1);
     }
 
     private boolean isValidRowCol(int row, int col) {
         return (row >= 1) && (col >= 1) && (row <= this.size) && (col <= this.size);
+    }
+
+    private boolean isConnectedToTop(int idx) {
+        int rootidx = this.connections.find(idx);
+        return this.siteStatus[rootidx][1];
+    }
+
+    private boolean isConnectedToBot(int idx) {
+        int rootidx = this.connections.find(idx);
+        return this.siteStatus[rootidx][2];
     }
 
     // opens the site (row, col) if it is not open already
@@ -33,33 +45,46 @@ public class Percolation {
         if (isValidRowCol(row, col)) {
             if (!isOpen(row, col)) {
                 int currentidx = getIdx(row, col);
-               this.sites[currentidx]=true;
+                this.siteStatus[currentidx][0] = true;
                 this.numberOpenSites++;
+
                 if (row == 1) {
-                   this.grid.union(currentidx, 0);
+                    this.siteStatus[currentidx][1] = true;
                 }
+
                 if (row == this.size) {
-                   this.grid.union(currentidx, this.size * this.size + 1);
+                    this.siteStatus[currentidx][2] = true;
                 }
+
                 if (isValidRowCol(row - 1, col) && isOpen(row - 1, col)) {
-                        int leftidx = getIdx(row-1, col);
-                       this.grid.union(currentidx, leftidx);
+                    int leftidx = getIdx(row-1, col);
+                    this.siteStatus[currentidx][1] = this.siteStatus[currentidx][1] || isConnectedToTop(leftidx);
+                    this.siteStatus[currentidx][2] = this.siteStatus[currentidx][2] || isConnectedToBot(leftidx);
+                    this.connections.union(currentidx, leftidx);
                 }
                 
                 if (isValidRowCol(row + 1, col) && isOpen(row + 1, col)) {
-                        int rightidx = getIdx(row+1, col);
-                       this.grid.union(currentidx, rightidx);
+                    int rightidx = getIdx(row+1, col);
+                    this.siteStatus[currentidx][1] = this.siteStatus[currentidx][1] || isConnectedToTop(rightidx);
+                    this.siteStatus[currentidx][2] = this.siteStatus[currentidx][2] || isConnectedToBot(rightidx);
+                    this.connections.union(currentidx, rightidx);
                 }
                 
                 if (isValidRowCol(row, col - 1) && isOpen(row, col - 1)) {
-                        int upidx = getIdx(row, col-1);
-                       this.grid.union(currentidx, upidx);
+                    int upidx = getIdx(row, col-1);
+                    this.siteStatus[currentidx][1] = this.siteStatus[currentidx][1] || isConnectedToTop(upidx);
+                    this.siteStatus[currentidx][2] = this.siteStatus[currentidx][2] || isConnectedToBot(upidx);
+                    this.connections.union(currentidx, upidx);
                 }
 
                 if (isValidRowCol(row, col + 1) && isOpen(row, col + 1)) {
-                        int downidx = getIdx(row, col+1);
-                       this.grid.union(currentidx, downidx);
+                    int downidx = getIdx(row, col+1);
+                    this.siteStatus[currentidx][1] = this.siteStatus[currentidx][1] || isConnectedToTop(downidx);
+                    this.siteStatus[currentidx][2] = this.siteStatus[currentidx][2] || isConnectedToBot(downidx);
+                    this.connections.union(currentidx, downidx);
                 }
+
+                this.percolates = this.percolates || (this.siteStatus[currentidx][1] && this.siteStatus[currentidx][2]);
             }
         }
         else {
@@ -70,7 +95,7 @@ public class Percolation {
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         if (isValidRowCol(row, col)) {
-            return this.sites[getIdx(row, col)];
+            return this.siteStatus[getIdx(row, col)][0];
         }
         else {
             throw new IllegalArgumentException();
@@ -80,32 +105,37 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         if (isValidRowCol(row, col)) {
-            return (isOpen(row, col) && (grid.find(getIdx(row, col)) == this.grid.find(0)));
+            return (isOpen(row, col) && isConnectedToTop(getIdx(row, col)));
         }
         else {
             throw new IllegalArgumentException();
         }
     }
 
-    // returns the number of openthis.sites
+    // returns the number of open sites
     public int numberOfOpenSites() {
         return this.numberOpenSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return (grid.find(0) == grid.find(this.size * this.size + 1));
+        return this.percolates;
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation my_percolation = new Percolation(2);
-        StdOut.println(my_percolation.percolates());
-        my_percolation.open(1, 1);
-        StdOut.println(my_percolation.percolates());
-        my_percolation.open(1, 2);
-        StdOut.println(my_percolation.percolates());
-        my_percolation.open(2, 1);
-        StdOut.println(my_percolation.percolates());
+        Percolation myPercolation = new Percolation(3);
+        myPercolation.open(1, 3);
+        StdOut.println(myPercolation.isFull(1, 3));
+        myPercolation.open(2, 3);
+        StdOut.println(myPercolation.isFull(2, 3));
+        myPercolation.open(3, 3);
+        StdOut.println(myPercolation.isFull(3, 3));
+        myPercolation.open(3, 1);
+        StdOut.println(myPercolation.isFull(3, 1));
+        myPercolation.open(2, 1);
+        StdOut.println(myPercolation.isFull(2, 2));
+        myPercolation.open(1, 1);
+        StdOut.println(myPercolation.isFull(1, 1));
     }
 }
--- CourseraAssignments/percolation/PercolationStats.java
+++ CourseraAssignments/percolation/PercolationStats.java
@@ -3,14 +3,16 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private double estimates[];
-    private double confidence95 = 1.96;
+    private static final double CONFIDENCE_95 = 1.96;
+    private final double[] estimates;
     
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
         estimates = new double[trials];
-        for(int t=0; t<trials; t++){
+        for (int t = 0; t < trials; t++) {
             Percolation experiment = new Percolation(n);
-            StdOut.println("Starting trial...");
             while (!experiment.percolates()) {
                 int row = StdRandom.uniform(n)+1;
                 int col = StdRandom.uniform(n)+1;
@@ -22,7 +24,6 @@ public class PercolationStats {
             double total = n * n;
             double estimate = sites/total;
             estimates[t] = estimate;
-            StdOut.println("Trial Complete.");
         }
     }
 
@@ -35,20 +36,22 @@ public class PercolationStats {
     }
 
     public double confidenceLo() {
-        return mean() - confidence95*stddev() / Math.sqrt(estimates.length);
+        return mean() - CONFIDENCE_95*stddev() / Math.sqrt(estimates.length);
     }
 
     public double confidenceHi() {
-        return mean() + confidence95*stddev() / Math.sqrt(estimates.length);
+        return mean() + CONFIDENCE_95*stddev() / Math.sqrt(estimates.length);
     }
     
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int T = Integer.parseInt(args[1]);
-        PercolationStats my_experiment = new PercolationStats(n, T);
-        StdOut.println(my_experiment.mean());
-        StdOut.println(my_experiment.stddev());
-        StdOut.println(my_experiment.confidenceLo());
-        StdOut.println(my_experiment.confidenceHi());
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats myExperiment = new PercolationStats(n, trials);
+        String mean = String.format("mean                    = %f", myExperiment.mean());
+        String std = String.format("stddev                  = %f", myExperiment.stddev());
+        String conf = String.format("95%% confidence interval = [%f,%f]", myExperiment.confidenceLo(), myExperiment.confidenceHi());
+        StdOut.println(mean);
+        StdOut.println(std);
+        StdOut.println(conf);
     }
 }
