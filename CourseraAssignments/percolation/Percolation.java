218d838344818bb708cbf1cff903bc35e3dbc6b7
Jesse Moeller <jessemmoeller@gmail.com>
2021-06-23 19:40:23 -0500

    removing superfluous lines

--- CourseraAssignments/percolation/Percolation.java
+++ CourseraAssignments/percolation/Percolation.java
@@ -30,23 +30,9 @@ public class Percolation {
         return (row >= 1) && (col >= 1) && (row <= this.size) && (col <= this.size);
     }
 
-    private void connectToTop(int idx){
-        int rootidx = this.connections.find(idx);
-        this.siteStatus[rootidx][1] = true;
-    }
-    private void connectToBot(int idx){
-        int rootidx = this.connections.find(idx);
-        this.siteStatus[rootidx][2] = true;
-    }
-
     private boolean connectedToTop(int idx) {
         int rootidx = this.connections.find(idx);
         return this.siteStatus[rootidx][1];
-    }
-
-    private boolean connectedToBot(int idx) {
-        int rootidx = this.connections.find(idx);
-        return this.siteStatus[rootidx][2];
     }
 
     // opens the site (row, col) if it is not open already
@@ -55,34 +41,36 @@ public class Percolation {
 
             // get the index of current node for WFU/status array lookup
             int currentidx = this.getIdx(row, col);
+            int rootcurrentidx = this.connections.find(currentidx);
 
             // modify the status to be open
             this.siteStatus[currentidx][0] = true;
+            this.numberOpenSites++;
 
-            int neighbors[][] = { {row-1, col}, {row+1, col}, {row, col-1}, {row, col+1} };
+            int[][] neighbors = { {row-1, col}, {row+1, col}, {row, col-1}, {row, col+1} };
 
             // we suppose the neighbors are not connected to top or bottom until proven otherwise
             boolean nbsConnectToTop = false;
             boolean nbsConnectToBot = false;
 
             // gather evidence and change those variables if counterexamples arise
-            for (int i = 0; i<4; i++) {
+            for (int i = 0; i < neighbors.length; i++) {
                 int nRow = neighbors[i][0];
                 int nCol = neighbors[i][1];
                 if (this.isValidRowCol(nRow, nCol)) {
                     int neighboridx = this.getIdx(nRow, nCol);
-                    if (this.connectedToTop(neighboridx)) {
+                    int rootidx = this.connections.find(neighboridx);
+                    if (this.siteStatus[rootidx][1]) {
                         nbsConnectToTop = true;
                     }
-
-                    if (this.connectedToBot(neighboridx)) {
-                        nbsConnectToBot = true;
+                    if (this.siteStatus[rootidx][2]) {
+                        nbsConnectToTop = true;
                     }
                 }
             }
 
             // Go back to the neighbors and make connections.
-            for (int i = 0; i<4; i++) {
+            for (int i = 0; i < neighbors.length; i++) {
                 int nRow = neighbors[i][0];
                 int nCol = neighbors[i][1];
                 if (this.isValidRowCol(nRow, nCol)) {
@@ -96,18 +84,18 @@ public class Percolation {
             // if the neighbors were originally connected to the top, or if you're just in the top row, then we connect to the top.
             // note that this has the effect of changing the full status of the neighbors, too.
             if (nbsConnectToTop || row == 1) {
-                this.connectToTop(currentidx);
+                this.siteStatus[rootcurrentidx][1] = true;
             }
 
             // ditto for the bottom
             if (nbsConnectToBot || row == this.size) {
-                this.connectToBot(currentidx);
+                this.siteStatus[rootcurrentidx][2] = true;
             }
 
             // detect percolation
             // note that this works, you're only connected to the top if either you're already at the top, or your neighbors are connected to the top.
             // (and, similarly, same for connecting to the bottom)
-            if ( (nbsConnectToTop || row == 1) && (nbsConnectToBot || row == this.size) ) {
+            if ((nbsConnectToTop || row == 1) && (nbsConnectToBot || row == this.size)) {
                 this.percolates = true;
             }
 
@@ -126,7 +114,7 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (isValidRowCol(row,col)) {
+        if (isValidRowCol(row, col)) {
             return this.connectedToTop(getIdx(row, col));
         }
         else {
