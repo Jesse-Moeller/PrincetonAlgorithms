5f041f9b1104c9ef7c893339c37c0346b69c7cc7
Jesse Moeller <jessemmoeller@gmail.com>
2021-06-30 19:26:52 -0500

    reorganization

---
+++ Exercises/Week2/LinkedStackOfStrings.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.*;
+
+public class LinkedStackOfStrings {
+    
+    private Node first = null;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(String item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    public String pop() {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        LinkedStackOfStrings myStack = new LinkedStackOfStrings();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].strip() == "-") {
+                StdOut.println("its poppin");
+                myStack.pop();
+            }
+            else {
+                myStack.push(args[i]);
+            }
+        }
+        while (!myStack.isEmpty()) {
+            StdOut.println(myStack.pop());
+        }
+    }
+}
